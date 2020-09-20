99d0dc7c9e01b3169a2a1851a88382b717cefbca
Adib234 <adibadib42@hotmail.com>
2020-09-19 22:54:24 -0400

    first commmit, completed landing page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="hero">
+    <div class="parent-1">
+      <h1 class="title is-1">Compare two animes! :)</h1>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <b-field class="label" label="Anime 1">
+          <b-input value="Enter the first anime!"></b-input>
+        </b-field>
+      </div>
+      <div class="column">
+        <b-field class="label" label="Anime 2">
+          <b-input value="Enter the second anime!"></b-input>
+        </b-field>
+      </div>
+    </div>
+    <div class="button-spacing">
+      <b-button class="button" type="is-primary">Compare!</b-button>
+    </div>
+  </section>
+</template>
+
+<script>
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+Vue.use(Buefy);
+export default {
+  methods: {},
+};
+</script>
+
+<style>
+.title.is-1 {
+  text-align: center;
+}
+.parent-1 {
+  margin-top: 10%;
+}
+.columns {
+  margin-top: 2%;
+  margin-right: 10%;
+  margin-left: 10%;
+}
+.label {
+  text-align: center;
+}
+.button {
+  width: 10%;
+}
+.button-spacing {
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { Button, Input } from "buefy";
+import "buefy/dist/buefy.css";
+Vue.use(Button);
+Vue.use(Input);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
