7ab24208e14b250968093b657b019a816558524e
Adib234 <adibadib42@hotmail.com>
2020-09-25 23:19:59 -0400

    Made successful calls but updates without me wanting to

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,21 +3,26 @@
     <div class="parent-1">
       <h1 class="title is-1">Compare two animes! :)</h1>
     </div>
+
     <div class="columns">
       <div class="column">
         <b-field class="label" label="Anime 1">
-          <b-input value="Enter the first anime!"></b-input>
+          <b-input value="Enter the first anime!" v-model="anime1"></b-input>
         </b-field>
       </div>
       <div class="column">
         <b-field class="label" label="Anime 2">
-          <b-input value="Enter the second anime!"></b-input>
+          <b-input value="Enter the second anime!" v-model="anime2"></b-input>
         </b-field>
       </div>
     </div>
     <div class="button-spacing">
-      <b-button class="button" type="is-primary">Compare!</b-button>
+      <b-button class="button" type="is-primary" @click="checkComplete"
+        >Compare!</b-button
+      >
     </div>
+
+    <Info :anime1="anime1" :anime2="anime2" v-if="success"></Info>
   </section>
 </template>
 
@@ -25,9 +30,41 @@
 import Vue from "vue";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
+import Info from "./Info.vue";
 Vue.use(Buefy);
+
 export default {
-  methods: {},
+  components: {
+    Info,
+  },
+  data() {
+    return {
+      anime1: "",
+      anime2: "",
+      success: false,
+    };
+  },
+
+  methods: {
+    checkComplete() {
+      if (this.anime1.length > 0 && this.anime2.length > 0) {
+        this.success = true;
+        return this.$buefy.toast.open({
+          message: "Yay, just a moment now!",
+          type: "is-success",
+          position: "is-bottom",
+          duration: 3000,
+        });
+      }
+      this.success = false;
+      return this.$buefy.toast.open({
+        duration: 3000,
+        message: `Please fill out both fields`,
+        position: "is-bottom",
+        type: "is-danger",
+      });
+    },
+  },
 };
 </script>
 
---
+++ src/components/Info.vue
@@ -0,0 +1,56 @@
+<template>
+  <section>
+    <img :src="url1" alt="./assets/notFound.png" />
+    <img :src="url2" alt="./assets/notFound.png" />
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: {
+    anime1: String,
+    anime2: String,
+  },
+  data() {
+    return {
+      url1: "",
+      url2: "",
+      error: "",
+    };
+  },
+  methods: {
+    animeFind(anime, data) {
+      axios
+        .get(`https://api.jikan.moe/v3/search/anime?q=${anime}`)
+        .then((response) => {
+          const id = response.data["results"][0]["mal_id"];
+          axios
+            .get(`https://api.jikan.moe/v3/anime/${id}`)
+            .then((response) => (this[data] = response.data["image_url"]));
+        })
+        .catch((error) => {
+          this.error = error; // take care of this later
+        });
+    },
+  },
+
+  watch: {
+    anime1: {
+      immediate: true,
+      // eslint-disable-next-line no-unused-vars
+      handler(newVal, oldVal) {
+        this.animeFind(newVal, "url1");
+      },
+    },
+    anime2: {
+      immediate: true,
+      // eslint-disable-next-line no-unused-vars
+      handler(newVal, oldVal) {
+        this.animeFind(newVal, "url2");
+      },
+    },
+  },
+};
+</script>
