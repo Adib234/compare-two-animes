595da8559f823d172cb9f8b5172e41db9aa4e500
Adib234 <adibadib42@hotmail.com>
2020-10-06 21:23:08 -0400

    halfway done chat ui

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,9 +1,17 @@
 <template>
   <section class="content">
     <div class="container2 has-text-centered is-size-2">Anime Title Chat</div>
-    <div class="columns">
-      <div class="column is-one-fifth">is-four-fifths</div>
-      <div class="column">Auto</div>
+    <div class="hero is-fullheight">
+      <div class="columns">
+        <div class="column is-one-fifth">is-four-fifths</div>
+        <div class="column">
+          <div class="messages"></div>
+          <div class="bottom">
+            <input class="input" type="text" placeholder="Text input" />
+            <button class="button">Send!</button>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -20,6 +28,28 @@ export default {};
   border-right: 1px solid;
 }
 .columns {
-  margin-top: auto;
+  margin-top: 0;
+  min-height: inherit;
+}
+
+.hero.is-fullheight {
+  min-height: 91vh;
+  align-items: stretch;
+  display: flex;
+  flex-direction: column;
+}
+.footer {
+  padding: 0;
+}
+.messages {
+  min-height: 82vh;
+}
+.input {
+  margin-left: 1rem;
+  margin-right: 2rem;
+}
+.bottom {
+  display: flex;
+  margin-right: 1rem;
 }
 </style>
